148949bb09a43d85af32a54c991d3bead981cb83
FrelNewBew <45685611+FrelNewBew@users.noreply.github.com>
2018-12-09 20:46:14 +0300

    initial commit

--- js/js.nav.js
+++ js/js.nav.js
@@ -2,7 +2,7 @@
 $(document).ready(function(){
  $('.reg_wr').on('mouseover', function() {
     $('.img_wrap').addClass('hovered');	
-    $(".hovered").css('filter','blur(10px)');   
+    $(".hovered").css('filter','blur(4px)');   
 });
    $('.reg_wr').on('mouseout', function() {
    	$(".hovered").css('filter','blur(0px)');
@@ -11,5 +11,20 @@ $(document).ready(function(){
 
 });
 
-
-
+$(document).ready(function() {
+var apocurelem = $('.lg');
+var apolet = apocurelem.text().split('');
+for(i in apolet) {
+apolet[i] = '<span>' + apolet[i] + '</span>';
+};
+var text = apolet.join("");
+apocurelem.html(text);
+ 
+var apocolorarr = ['#EA2027', '#0652DD', '#009432', '#006266', '#1B1464'];
+var min = 0;
+var max = apocolorarr.length-1;
+$('.lg span').each(function(index) {
+var aporandom = min + Math.floor(Math.random() * (max + 1 - min));
+$(this).css('color', apocolorarr[aporandom]);
+});
+});
